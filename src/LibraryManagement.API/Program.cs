93031097be72a535b8f6b6cda9ea6eeb3804f646
Richmond Boakye <richmondboakye0017@gmail.com>
2026-01-21 18:05:15 +0100

    Implemented the primary dependencies for the project.

--- src/LibraryManagement.API/Program.cs
+++ src/LibraryManagement.API/Program.cs
@@ -1,19 +1,24 @@
-//using LibraryManagement.Application;
-//using LibraryManagement.Infrastructure;
+using LibraryManagement.Application;
+using LibraryManagement.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ src/LibraryManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LibraryManagement.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+    this IServiceCollection services,
+    IConfiguration configuration)
+    {
+        //// Database
+        //services.AddDbContext<LibraryDbContext>(options =>
+        //    options.UseSqlServer(
+        //        configuration.GetConnectionString("DefaultConnection")));
+
+        //// Repositories
+        //services.AddScoped<IBookRepository, SqlBookRepository>();
+        //services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        //// Caching (when you add Redis later)
+        //// services.AddStackExchangeRedisCache(options =>
+        ////     options.Configuration = configuration.GetConnectionString("Redis"));
+        //// services.AddScoped<ICacheService, RedisCacheService>();
+
+        return services;
+    }
+}
