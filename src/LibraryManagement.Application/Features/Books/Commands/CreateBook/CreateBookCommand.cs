2598213a1b677575a4ece43930e9b0b1d00fb177
Richmond Boakye <richmondboakye0017@gmail.com>
2026-01-22 00:12:43 +0100

    Have implemented bookcontroller methods

---
+++ src/LibraryManagement.Application/Features/Books/Commands/CreateBook/CreateBookCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Application.Features.Books.Commands.CreateBook;
+
+public  record CreateBookCommand(
+    string Title,
+    string Author,
+    string ISBN,
+    int PublishedYear
+) : IRequest<Guid>;
+
---
+++ src/LibraryManagement.Application/Features/Books/Queries/GetBookById/GetBookByIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using LibraryManagement.Application.Common.Interfaces;
+using LibraryManagement.Application.Features.Books.Queries.GetAllBooks;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.Application.Features.Books.Queries.GetBookById;
+
+public class GetBookByIdQueryHandler: IRequestHandler<GetBookByIdQuery, BookResponse?>
+{
+    private readonly IBookRepository _bookRepository;
+    public GetBookByIdQueryHandler(IBookRepository bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public async Task<BookResponse?> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+    {
+        var book = await _bookRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (book is null)
+            return null;
+
+        return new BookResponse(
+            book.Id,
+            book.Title,
+            book.Author,
+            book.ISBN,
+            book.PublishedYear
+        );
+    }
+}
