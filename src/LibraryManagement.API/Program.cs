a28876dd663277862085c0c30af21c107946b232
Richmond Boakye <richmondboakye0017@gmail.com>
2026-01-21 15:45:11 +0100

    I have installed dependencies for the Application namespace

--- src/LibraryManagement.API/Program.cs
+++ src/LibraryManagement.API/Program.cs
@@ -1,3 +1,5 @@
+//using LibraryManagement.Application;
+//using LibraryManagement.Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
---
+++ src/LibraryManagement.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using FluentValidation;
+using System.Runtime.CompilerServices;
+using FluentAssertions.Common;
+
+
+namespace LibraryManagement.Application;
+
+public static class DependencyInjection 
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // MediatR - handles commands/queries
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(assembly));
+
+        // FluentValidation - auto-register validators
+        services.AddValidatorsFromAssembly(assembly);
+
+        // AutoMapper 
+        services.AddAutoMapper(assembly);
+
+        return services;
+    }
+
+}
