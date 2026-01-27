985303de89fb81c2382e3d1d5ac958bf3583d85e
Richmond Boakye <richmondboakye0017@gmail.com>
2026-01-27 18:27:55 +0100

    I have done unit testing and they all succeeded.

---
+++ tests/LibraryManagement.UnitTests/Application/Features/Books/Queries/GetBookByIdQueryHandlerTests.cs
@@ -0,0 +1,120 @@
+﻿using FluentAssertions;
+using LibraryManagement.Application.Common.Interfaces;
+using LibraryManagement.Application.Features.Books.Queries.GetBookById;
+using LibraryManagement.UnitTests.Common.Fixtures;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagement.UnitTests.Application.Features.Books.Queries
+{
+    public class GetBookByIdQueryHandlerTests
+    {
+        private readonly Mock<IBookRepository> _bookRepositoryMock;
+        private readonly GetBookByIdQueryHandler _handler;
+
+        public GetBookByIdQueryHandlerTests()
+        {
+            _bookRepositoryMock = new Mock<IBookRepository>();
+            _handler = new GetBookByIdQueryHandler(_bookRepositoryMock.Object);
+        }
+
+        [Fact]
+        public async Task Handle_WithExistingBook_ShouldReturnBook()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            var book = BookFixtures.GetTestBook(id: bookId, title: "Test Book");
+
+            _bookRepositoryMock
+                .Setup(r => r.GetByIdAsync(bookId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(book);
+
+            var query = new GetBookByIdQuery(bookId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result!.Title.Should().Be("Test Book");
+        }
+
+        [Fact]
+        public async Task Handle_WithNonExistingBook_ShouldReturnNull()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+
+            _bookRepositoryMock
+                .Setup(r => r.GetByIdAsync(bookId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync((LibraryManagement.Domain.Entities.Book?)null);
+
+            var query = new GetBookByIdQuery(bookId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task Handle_ShouldMapAllPropertiesCorrectly()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+            var book = BookFixtures.GetTestBook(
+                id: bookId,
+                title: "Clean Code",
+                author: "Robert Martin",
+                isbn: "9780132350884",
+                publishedYear: 2008
+            );
+            book.SetCoverImage("https://example.com/cover.jpg");
+
+            _bookRepositoryMock
+                .Setup(r => r.GetByIdAsync(bookId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(book);
+
+            var query = new GetBookByIdQuery(bookId);
+
+            // Act
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            result.Should().NotBeNull();
+            result!.Id.Should().Be(bookId);
+            result.Title.Should().Be("Clean Code");
+            result.Author.Should().Be("Robert Martin");
+            result.ISBN.Should().Be("9780132350884");
+            result.PublishedYear.Should().Be(2008);
+            result.CoverImageUrl.Should().Be("https://example.com/cover.jpg");
+        }
+
+        [Fact]
+        public async Task Handle_ShouldCallRepositoryWithCorrectId()
+        {
+            // Arrange
+            var bookId = Guid.NewGuid();
+
+            _bookRepositoryMock
+                .Setup(r => r.GetByIdAsync(bookId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync((LibraryManagement.Domain.Entities.Book?)null);
+
+            var query = new GetBookByIdQuery(bookId);
+
+            // Act
+            await _handler.Handle(query, CancellationToken.None);
+
+            // Assert
+            _bookRepositoryMock.Verify(
+                r => r.GetByIdAsync(bookId, It.IsAny<CancellationToken>()),
+                Times.Once
+            );
+        }
+    }
+}
---
+++ tests/LibraryManagement.UnitTests/Domain/Entities/BookTests.cs
@@ -0,0 +1,182 @@
+﻿using FluentAssertions;
+using LibraryManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LibraryManagement.UnitTests.Domain.Entities
+{
+    public class BookTests
+    {
+        #region Create Tests
+
+        [Fact]
+        public void Create_WithValidData_ReturnsBook()
+        {
+            // Arrange
+            var title = "Clean Architecture";
+            var author = "Robert C. Martin";
+            var isbn = "978-0134494166";
+            var publishedYear = 2017;
+
+            // Act
+            var book = Book.Create(title, author, isbn, publishedYear);
+
+            // Assert
+            book.Should().NotBeNull();
+            book.Title.Should().Be(title);
+            book.Author.Should().Be(author);
+            book.ISBN.Should().Be(isbn);
+            book.PublishedYear.Should().Be(publishedYear);
+        }
+
+        [Fact]
+        public void Create_ShouldGenerateNewId()
+        {
+            // Arrange & Act
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+
+            // Assert
+            book.Id.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void Create_ShouldSetCreatedAt()
+        {
+            // Arrange
+            var beforeCreation = DateTime.UtcNow;
+
+            // Act
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+
+            // Assert
+            book.CreatedAt.Should().BeOnOrAfter(beforeCreation);
+            book.CreatedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(1));
+        }
+
+        [Fact]
+        public void Create_ShouldHaveNullUpdatedAt()
+        {
+            // Arrange & Act
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+
+            // Assert
+            book.UpdatedAt.Should().BeNull();
+        }
+
+        [Fact]
+        public void Create_ShouldHaveNullCoverImageUrl()
+        {
+            // Arrange & Act
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+
+            // Assert
+            book.CoverImageUrl.Should().BeNull();
+        }
+
+        #endregion
+
+        #region Update Tests
+
+        [Fact]
+        public void Update_WithValidData_ShouldUpdateProperties()
+        {
+            // Arrange
+            var book = Book.Create("Old Title", "Old Author", "1111111111", 2000);
+            var newTitle = "New Title";
+            var newAuthor = "New Author";
+            var newIsbn = "2222222222";
+            var newYear = 2023;
+
+            // Act
+            book.Update(newTitle, newAuthor, newIsbn, newYear);
+
+            // Assert
+            book.Title.Should().Be(newTitle);
+            book.Author.Should().Be(newAuthor);
+            book.ISBN.Should().Be(newIsbn);
+            book.PublishedYear.Should().Be(newYear);
+        }
+
+        [Fact]
+        public void Update_ShouldSetUpdatedAt()
+        {
+            // Arrange
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+            var beforeUpdate = DateTime.UtcNow;
+
+            // Act
+            book.Update("New Title", "New Author", "0987654321", 2023);
+
+            // Assert
+            book.UpdatedAt.Should().NotBeNull();
+            book.UpdatedAt.Should().BeOnOrAfter(beforeUpdate);
+        }
+
+        [Fact]
+        public void Update_ShouldNotChangeId()
+        {
+            // Arrange
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+            var originalId = book.Id;
+
+            // Act
+            book.Update("New Title", "New Author", "0987654321", 2023);
+
+            // Assert
+            book.Id.Should().Be(originalId);
+        }
+
+        [Fact]
+        public void Update_ShouldNotChangeCreatedAt()
+        {
+            // Arrange
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+            var originalCreatedAt = book.CreatedAt;
+
+            // Act
+            book.Update("New Title", "New Author", "0987654321", 2023);
+
+            // Assert
+            book.CreatedAt.Should().Be(originalCreatedAt);
+        }
+
+        #endregion
+
+        #region SetCoverImage Tests
+
+        [Fact]
+        public void SetCoverImage_WithValidUrl_ShouldSetCoverImageUrl()
+        {
+            // Arrange
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+            var coverUrl = "https://storage.blob.core.windows.net/covers/book.jpg";
+
+            // Act
+            book.SetCoverImage(coverUrl);
+
+            // Assert
+            book.CoverImageUrl.Should().Be(coverUrl);
+        }
+
+        [Fact]
+        public void SetCoverImage_ShouldSetUpdatedAt()
+        {
+            // Arrange
+            var book = Book.Create("Title", "Author", "1234567890", 2020);
+            var beforeUpdate = DateTime.UtcNow;
+
+            // Act
+            book.SetCoverImage("https://example.com/cover.jpg");
+
+            // Assert
+            book.UpdatedAt.Should().NotBeNull();
+            book.UpdatedAt.Should().BeOnOrAfter(beforeUpdate);
+        }
+
+        #endregion
+    }
+}
